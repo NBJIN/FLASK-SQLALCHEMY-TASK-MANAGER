8d76d33a6a43a65653ee05dbd17d5ac300bf86f5
noreen <noreennelligan1@gmail.com>
2022-12-28 11:25:39 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 from taskmanager import app
 
-if ___name___ == "___main___":
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
